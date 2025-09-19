2f958885de6560c84d3bbf102dc98a186eb6c18a
majny <majny24@gmail.com>
2025-09-19 10:00:58 +0200

    client server setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 const port = 5000
+
+
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
